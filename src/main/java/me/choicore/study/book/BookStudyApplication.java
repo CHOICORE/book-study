b0d529a52a5458b0248774657a936820e46d7881
CHOICORE <choijaehyeong93@gmail.com>
2024-01-01 17:02:31 +0900

    project: initial commit

---
+++ src/main/java/me/choicore/study/book/BookStudyApplication.java
@@ -0,0 +1,13 @@
+package me.choicore.study.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/choicore/study/book/BookStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.choicore.study.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
